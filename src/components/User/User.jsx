0ff549c36441c55e5f26363f4f0b56e2ad4f576e
unknown <143880221+FLORRIBA@users.noreply.github.com>
2024-09-02 11:13:00 -0300

    cambios

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,22 +1,26 @@
+import './User.css';
 
-const UserImage = '../../../public/images/Florencia.jpg';
+// Ajuste de la ruta de la imagen para que apunte directamente a la carpeta 'public'
+const UserImage = '/images/Florencia.jpg';
 
 const User = () => {
-    return (
-        <section id="home" className="user-container">
-            <div className="user-content">
-                <h1>Building digital futures: driving discovery and creativity</h1>
-                <p>
-                    As a full-stack developer, I try to excel at creating new experiences. Join this journey where ideas evolve to different digital places.
-                </p>
-            </div>
-            <div className="user-img">
-                <div>
-                    <img src={UserImage} alt="userImage" />
-                </div>
-            </div>
-        </section>
-    );
+	return (
+		<section id="home" className="user-container">
+			<div className="user-content">
+				<h1>Building digital futures: driving discovery and creativity</h1>
+				<p>
+					As a full-stack developer, I try to excel at creating new experiences. Join this journey where ideas evolve to different digital places.
+				</p>
+			</div>
+			<div className="user-img">
+				<div>
+					<img src={UserImage} alt="userImage" />
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default User;
+
+
